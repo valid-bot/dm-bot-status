0277bfc2fef78fb7acdffeb560cba7b3006922f1
Pixxth <61689690+Pixxth@users.noreply.github.com>
2020-05-05 07:06:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 let args = message.content.split(" ").slice(1);
 var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
+ m.send(`${m} ${argresult}`);
+   // pings user in dms and shows the message you sent.
 })
- message.channel.send(`\ ${message.guild.members.filter(m => m.presence.status !== 'online').size} Dms sent`); 
- message.delete(); 
+ message.channel.send(`\ ${message.guild.members.filter(m => m.presence.status !== 'online').size} has been sent to dms`); 
+ message.delete(); // shows you how many have been sent successfully.
 };     
 });
+
 
 
 client.on('ready',()=>{
     console.log(' Bot is online now!');
     let activities = [ `${client.guilds.size} servers!`, `${client.channels.size} channels!`, `${client.users.size} users!` ], i = 0;
-    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]} || $help `, { type: "WATCHING" }), 15000)
+    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
+     // just a quick stream command you can add more shit to it when you edit it (`${activities[i++ % activities.length]} || $help `
 
 
 
 });
 
-client.login("Njk4Nzk3NDI5ODAxMjIyMTU0.XpeKuw.glTvkugyCW-cF7069UJ8XSs-qkg");
+client.login("");
